31a381dbf1709acefb098c04cbbf7e01ef683c8a
mon0225 <elizabeta0225@hotmail.com>
2018-11-21 15:20:32 -0600

    orden

--- starter-code/src/component/Main.js
+++ starter-code/src/component/Main.js
@@ -38,36 +38,36 @@ class Grid3 extends Component{
                                             <br/>
                                             <label className="black-text text-darken">34679</label>
                                         </div>
-                                        <div className="column">
-                                            <label className="black-text text-darken font-weight-bold mt-2 ml-5"><span>FECHA</span></label>
+                                    <div className="column">
+                                        <label className="black-text text-darken font-weight-bold mt-2 ml-5"><span>FECHA</span></label>
                                         <br/>
-                                            <label className="black-text text-darken mt-2 ml-5">20/03/2018</label>
-                                        </div>
-                                        <div className="column">
-                                            <label className="black-text text-darken font-weight-bold mt-3"><span>TOTAL</span></label>
+                                        <label className="black-text text-darken mt-2 ml-5">20/03/2018</label>
+                                    </div>
+                                     <div className="column">
+                                        <label className="black-text text-darken font-weight-bold mt-3"><span>TOTAL</span></label>
                                         <br/>
-                                            <label className="black-text text-darken mt-2">$2,340 MXN</label>
-                                        </div>
-                                        <div className="column">
-                                            <label className="black-text text-darken font-weight-bold mt-3"><span>CANTIDAD</span></label>
+                                        <label className="black-text text-darken mt-2">$2,340 MXN</label>
+                                    </div>
+                                    <div className="column">
+                                        <label className="black-text text-darken font-weight-bold mt-3"><span>CANTIDAD</span></label>
                                         <br/>
-                                            <label className="black-text text-darken mt-2">3</label>
-                                        </div>
-                                        <div className="column">
-                                            <label className="black-text text-darken font-weight-bold mt-2"><span>ESTATUS</span></label>
+                                        <label className="black-text text-darken mt-2">3</label>
+                                    </div>
+                                     <div className="column">
+                                        <label className="black-text text-darken font-weight-bold mt-2"><span>ESTATUS</span></label>
                                         <br/>
-                                            <label className="black-text text-darken mt-2">Entregado</label>
-                                        </div>
+                                        <label className="black-text text-darken mt-2">Entregado</label>
+                                    </div>
                                         <div className="column is-1.5 padd">
                                             <label className="black-text text-darken font-weight-bold mt-2 ml-5"><span>ENVIAR A</span></label>
-                                        <br/>
+                                            <br/>
                                             <label className="black-text text-darken ml-5" style={{width:250}}>Lucía Susana Martínez Reyes</label>
                                         </div>
                                     </div>
                                 </div>
-                                </div>
-                                <div className="row ml-4" style={{width:940}}>
-                                <div className="card" style={{width:1000, height:200}} >
+                            </div>
+                        <div className="row ml-4" style={{width:940}}>
+                            <div className="card" style={{width:1000, height:200}} >
                                 <div className="columns">
                                     <div className="column is-2 padd">
                                         <h3 className="mt-4 ml-3 text-center"><span>ACCUVE OASIS</span></h3>
@@ -75,38 +75,70 @@ class Grid3 extends Component{
                                     </div>
                                 <div className="column is-2 padd">
                                     <p className="font-weight-bold mt-5 ml-5 text-center">CANTIDAD</p><br />
-                                <div className="select ml-5 mr-1">
-                                    <select>
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                    </select>
+                                    <div className="select ml-5 mr-1">
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                     </div>
+                                 </div>
+                                <div className="column is-2 ">
+                                    <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO UNITARIO</p>
+                                    <p className="mt-2 ml-4 text-center">$2,345</p>
+                                </div>
+                                <div className="column is-2">
+                                    <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO TOTAL</p>
+                                    <p className="mt-2 ml-4 text-center">$4,567</p>
+                                </div>
+                                <div className="column">
+                                    <br/>
+                                    <button className="btn btn-primary btn-medium waves-effect waves-light font-weight-bold mt-5 ml-4 text-center" style={{borderRadius:50}}>VOLVER A COMPRAR</button>
+                                </div>
                                 </div>
                             </div>
-                        <div className="column is-2 ">
-                            <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO UNITARIO</p>
-                            <p className="mt-2 ml-4 text-center">$2,345</p>
-                        </div>
-                        <div className="column is-2">
-                            <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO TOTAL</p>
-                            <p className="mt-2 ml-4 text-center">$4,567</p>
-                        </div>
-                        <div className="column">
-                        <br/>
-                        <button className="btn btn-primary btn-medium waves-effect waves-light font-weight-bold mt-5 ml-4 text-center" style={{borderRadius:50}}>VOLVER A COMPRAR</button>
-                        </div>
-                                </div>
-                            </div>
-                            <div className="card" style={{width:1000, height:200}} >
+                        <div className="card" style={{width:1000, height:200}} >
                             <div className="columns">
                                 <div className="column is-2 padd">
                                     <h3 className="mt-4 ml-3 text-center"><span>LENTES</span></h3>
                                     <img className="mt-4 ml-4" src="/lentes.png" alt="accuve" />
                                 </div>
+                                <div className="column is-2 padd">
+                                    <p className="font-weight-bold mt-5 ml-5 text-center">CANTIDAD</p><br />
+                                    <div className="select ml-5 mr-1">
+                                        <select>
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>
+                                    </div>
+                                </div>
+                             <div className="column is-2 ">
+                                <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO UNITARIO</p>
+                                <p className="mt-2 ml-4 text-center">$2,345</p>
+                            </div>
+                            <div className="column is-2">
+                                <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO TOTAL</p>
+                                <p className="mt-2 ml-4 text-center">$4,567</p>
+                            </div>
+                            <div className="column">
+                                <br/>
+                                <button className="btn btn-primary btn-medium waves-effect waves-light font-weight-bold mt-5 ml-4 text-center" style={{borderRadius:50}}>VOLVER A COMPRAR</button>
+                            </div>
+                            </div>
+                        </div>
+                    <div className="card" style={{width:1000, height:200}} >
+                        <div className="columns">
                             <div className="column is-2 padd">
-                                <p className="font-weight-bold mt-5 ml-5 text-center">CANTIDAD</p><br />
+                                <h3 className="mt-4 ml-3 text-center"><span>ACCUVE OASIS</span></h3>
+                                <img className="mt-4 ml-4" src="/accuve.png" alt="accuve" />
+                            </div>
+                        <div className="column is-2 padd">
+                            <p className="font-weight-bold mt-5 ml-5 text-center">CANTIDAD</p><br />
                             <div className="select ml-5 mr-1">
                                 <select>
                                     <option>1</option>
@@ -117,58 +149,24 @@ class Grid3 extends Component{
                                 </select>
                             </div>
                         </div>
-                    <div className="column is-2 ">
-                        <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO UNITARIO</p>
-                        <p className="mt-2 ml-4 text-center">$2,345</p>
-                    </div>
-                    <div className="column is-2">
-                        <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO TOTAL</p>
-                        <p className="mt-2 ml-4 text-center">$4,567</p>
-                    </div>
-                    <div className="column">
-                    <br/>
-                    <button className="btn btn-primary btn-medium waves-effect waves-light font-weight-bold mt-5 ml-4 text-center" style={{borderRadius:50}}>VOLVER A COMPRAR</button>
-                    </div>
-                            </div>
+                        <div className="column is-2 ">
+                            <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO UNITARIO</p>
+                            <p className="mt-2 ml-4 text-center">$2,345</p>
                         </div>
-                        <div className="card" style={{width:1000, height:200}} >
-                        <div className="columns">
-                            <div className="column is-2 padd">
-                                <h3 className="mt-4 ml-3 text-center"><span>ACCUVE OASIS</span></h3>
-                                <img className="mt-4 ml-4" src="/accuve.png" alt="accuve" />
-                            </div>
-                        <div className="column is-2 padd">
-                            <p className="font-weight-bold mt-5 ml-5 text-center">CANTIDAD</p><br />
-                        <div className="select ml-5 mr-1">
-                            <select>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                            </select>
+                        <div className="column is-2">
+                            <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO TOTAL</p>
+                            <p className="mt-2 ml-4 text-center">$4,567</p>
+                        </div>
+                        <div className="column">
+                            <br/>
+                            <button className="btn btn-primary btn-medium waves-effect waves-light font-weight-bold mt-5 ml-4 text-center" style={{borderRadius:50}}>VOLVER A COMPRAR</button>
+                        </div>
                         </div>
                     </div>
-                <div className="column is-2 ">
-                    <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO UNITARIO</p>
-                    <p className="mt-2 ml-4 text-center">$2,345</p>
-                </div>
-                <div className="column is-2">
-                    <p className="font-weight-bold mt-5 ml-5 text-center">PRECIO TOTAL</p>
-                    <p className="mt-2 ml-4 text-center">$4,567</p>
-                </div>
-                <div className="column">
-                <br/>
-                <button className="btn btn-primary btn-medium waves-effect waves-light font-weight-bold mt-5 ml-4 text-center" style={{borderRadius:50}}>VOLVER A COMPRAR</button>
-                </div>
-                        </div>
-                    </div>
-                                </div>
-                         
-                        
+                </div>       
             </div>
         </div>      
-</div>
+    </div>
 <div className="col-4">
     <div className="row" style={{width:450, height:1158}}>
         <div className="col-md-12">
